6bc136aa8dfd064b22126a237a2be8e48ef832dd
Jeremy Lincoln <bossco28@gmail.com>
2017-03-22 10:25:15 -0400

    Edit user is live now

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -7,6 +7,10 @@
         <th>Lastname</th>
         <th>Email</th>
         <th>Role</th>
+        <th>Admin</th>
+        <th>Subscriber</th>
+        <th>Edit</th>
+        <th>Delete</th>
 <!--        <th>Date</th>-->
     </tr>
     </thead>
@@ -47,6 +51,7 @@
 
         echo "<td><a href='users.php?change_to_admin=$user_id'>Admin</a> </td>";
         echo "<td><a href='users.php?change_to_sub=$user_id'>Subscriber</a> </td>";
+        echo "<td><a href='users.php?source=edit_user&edit_user=$user_id'>Edit</a> </td>";
         echo "<td><a href='users.php?delete=$user_id'>Delete</a> </td>";
 
         echo "</tr>";
